9107608ed7b3f2173627a71442ef1d3ae2b924ae
nharsch <nigelharsch@gmail.com>
2015-02-08 16:54:30 -0500

    functional test changes

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -11,8 +12,37 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.quit()
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
+		#test URL
 		self.browser.get('http://localhost:8000')
+
+		# page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_elements_by_tag_name('h1').text 
+		self.assertIn('To-Do', header_text)
+
+
+		# user is invited to enter a to-do item
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+				inputbox.get_attribute('placeholder'),
+				'Enter a to-do item'
+		)
+
+		# User types "Buy Peacock feathers" into a text box
+		inputbox.send_keys('Buy peacock feathers')
+
+		# user hits enter, the page updates, page lists:
+		# "1: Buy peacock feathers" as an item in a to-do list table
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows)
+		)
+
+		# still a text box inviting user to add another item, user enters:
+		# "Use peacock feathers to make a fly"
 		self.fail('Finish the test')
 
 if __name__ == '__main__':
