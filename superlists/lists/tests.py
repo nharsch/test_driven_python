aad069edc391fd775fa0219d4a98cbb4de54f278
nharsch <nigelharsch@gmail.com>
2015-02-08 22:14:48 -0500

    left off chapter 5

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -12,10 +12,21 @@ class HomePageTest(TestCase):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
 
+
 	def test_home_page_returns_correct_html(self):
 		request = HttpRequest()
+		request.POST['item_text'] = 'A new list item'
+
 		response = home_page(request)
+
 		expected_html = render_to_string('home.html')
-		self.assertTrue(response.content.startswith(b'<html>'))
 		self.assertIn(b'<title>To-Do lists</title>', response.content)
 		self.assertTrue(response.content.decode(), expected_html)
+
+	def test_home_page_can_save_a_POST_request(self):
+		request = HttpRequest()
+		request.method = 'POST'
+
+		response = home_page(request)
+		
+		self.assertIn('A new list item', response.content.decode())
