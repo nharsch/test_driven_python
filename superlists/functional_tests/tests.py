c52cd32e1cf39d9d1f543497636304807163aa27
nharsch <nigelharsch@gmail.com>
2015-03-04 21:07:13 -0500

    added functional tests dir and moved FT. use LiveServerTestCase

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -2,8 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 import time
+from django.test import LiveServerTestCase
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -19,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		#test URL
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 
 		# page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
@@ -57,5 +58,3 @@ class NewVisitorTest(unittest.TestCase):
 		# explanatory text to that effect.
 		self.fail('Finish the test')
 
-if __name__ == '__main__':
-	unittest.main()
