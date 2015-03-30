24d80fb50bf87aa2d596b25bcc47164385aedf1b
nharsch <nigelharsch@gmail.com>
2015-03-29 22:00:46 -0400

    now with listview

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -5,20 +5,21 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-	if request.method == 'POST':
-		Item.objects.create(text=request.POST['item_text'])
-		return redirect('/lists/the-only-list-in-the-world/')
+    return render(request, 'home.html')
 
-	return render(request, 'home.html')
-		
+
 def view_list(request):
-	items = Item.objects.all()
-	return render(request, 'list.html', {'items':items})
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items': items})
 
 
+def new_list(request):
+    if request.POST:
+        Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
 
-	
-	# if request.method == 'POST':
-	# 	return render(request, 'home.html', {
-	# 		'new_item_text': request.POST['item_text'],
-	# 		})
+
+# if request.method == 'POST':
+# 	return render(request, 'home.html', {
+# 		'new_item_text': request.POST['item_text'],
+# 		})
