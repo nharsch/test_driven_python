5cda1c48aa6d70a2170da9067e4a2a02332ef1c2
nharsch <nigelharsch@gmail.com>
2015-04-29 22:08:00 -0400

    Adjust new list view to do model validation

--- lists/views.py
+++ lists/views.py
@@ -25,7 +25,7 @@ def new_list(request):
         list_.delete()
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error": error})
-    return redirect('/lists/%d/' % (list_.id,))
+    return redirect('/lists/{}/'.format(list_.id,))
 
 
 def add_item(request, list_id):
