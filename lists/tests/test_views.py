ab8cd6901fe42387e6c0ef98771734434c060a0a
nharsch <nigelharsch@gmail.com>
2015-05-05 22:26:35 -0400

    removed uneccessary tests

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -11,16 +11,6 @@ from lists.views import home_page
 class HomePageTest(TestCase):
     maxDiff = None
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
-
     def test_home_page_renders_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
@@ -28,12 +18,6 @@ class HomePageTest(TestCase):
     def test_home_page_uses_item_form(self):
         response = self.client.get('/')
         self.assertIsInstance(response.context['form'], ItemForm)
-
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        expected_html = render_to_string('home.html', {'form': ItemForm()})
-        self.assertMultiLineEqual(response.content.decode(), expected_html)
 
 class NewListTest(TestCase):
 
